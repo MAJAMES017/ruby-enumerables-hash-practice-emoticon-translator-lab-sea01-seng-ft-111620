f42be5ff9e8d68a105ae10563f5086c9d3cfd176
michael james <majames071@gmail.com>
2020-11-08 21:14:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
 
 def load_library
-  # code goes here
+  final_hash = {}
+    YAML.load_file(path).each do |key, value|
+      final_hash[key] = {}
+      final_hash[key][:english] = value[0]
+      final_hash[key][:japanese] = value[1]
+    end
+    final_hash
 end
 
 def get_japanese_emoticon
